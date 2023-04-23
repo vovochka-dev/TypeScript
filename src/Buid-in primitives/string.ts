a6effb7f7409359319d11fc5aec545b37c6ae822
Volodymyr Korchemnyi <volodymyr.korchemnyi@gmail.com>
2023-04-23 15:12:24 +0300

    fix: string.ts is commonjs module

--- src/Buid-in primitives/string.ts
+++ src/Buid-in primitives/string.ts
@@ -4,15 +4,17 @@ let text2: string;
 //@ts-expect-error 'const' declarations must be initialized.
 const text3: string;
 
-//Implicit initialization
+// Implicit initialization
 let stringValue: string = "some text";
-var text4 = stringValue; //string
-let text5 = stringValue; //string
-const text6 = stringValue; //string
-const text7 = "some text"; //some text
+var text4 = stringValue; // string
+let text5 = stringValue; // string
+const text6 = stringValue; // string
+const text7 = "some text"; // some text
 
-//Explicit initialization
-var text8: string = stringValue; //string Redundant type declaration
-let text9: string = stringValue; //string Redundant type declaration
-const text10: string = stringValue; //string Redundant type declaration
-const text11: string = "some text"; //string. Without type declaration: "some text"
+// Explicit initialization
+var text8: string = stringValue; // string Redundant type declaration
+let text9: string = stringValue; // string Redundant type declaration
+const text10: string = stringValue; // string Redundant type declaration
+const text11: string = "some text"; // string Without type declaration: "some text"
+
+export {};
